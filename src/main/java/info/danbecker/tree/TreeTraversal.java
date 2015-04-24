06b8c4d56504cfeabf449ee3f023a2bbdb6d2070
Dan Becker <dabecker@paypal.com>
2015-04-24 16:12:07 -0500

    Comparison code

--- src/main/java/info/danbecker/tree/TreeTraversal.java
+++ src/main/java/info/danbecker/tree/TreeTraversal.java
@@ -1,6 +1,8 @@
 package info.danbecker.tree;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.AbstractMap;
 import java.util.Properties;
 import java.util.Random;
 import java.util.List;
@@ -17,7 +19,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * An app to demonstrate tree traversal and costs. 
+ * An app to demonstrate sparse tree traversal and costs.
+ * Users get to set tree size, node sizes (empty, just key, and data),
+ * fill percentage, and number of accesses.
+ * The program then spits out some sizes and benchmarks.
+ * <p>
+ * For demonstration purposes, this app uses a binary tree, but
+ * can be updated to work with N trees.
+ * <p>
+ * Implementation details are contained below.
+ * <p>
  * @author <a href="mailto://dan@danbecker.info>Dan Becker</a>
  */
 public class TreeTraversal {
@@ -27,10 +38,13 @@ public class TreeTraversal {
 	public static boolean debug = false;
 	
     protected static Properties properties;
+	protected static List<String> nodes; 
 
     protected static Random random = new Random();
     
     public static int numKeys;
+    public static int numNodes;
+    public static int popNodes;
     public static int nodeReadCost;
     public static int nodeDataSize;
     public static int nodeKeySize;
@@ -38,6 +52,7 @@ public class TreeTraversal {
     public static int treeFillPercent;
     public static int numAccesses;
 
+    public static final String DATA = "DATA";
 	protected static Logger logger = LoggerFactory.getLogger(TreeTraversal.class);
     
     static {
@@ -89,12 +104,20 @@ public class TreeTraversal {
 	    execute();
 	    
 		// conclude and end
-		if (verbose) {
-	       long elapsedTime = System.currentTimeMillis() - startTime;
-	       System.out.println( "application elapsed time=" + format( elapsedTime ));       
-		}
+		// if (verbose) {
+	    //    long elapsedTime = System.currentTimeMillis() - startTime;
+	    //    System.out.println( "application elapsed time=" + format( elapsedTime ));       
+		// }
 	}
 
+	/** 
+	 * This method reads properties, builds an implementation, and runs tests. 
+	 * The implementation builds an array-list to contain a binary tree.
+	 * The top of the tree is at array position 0.
+	 * The second level of the tree is at array positions 1, 2,
+	 * The third level of the tree is at array positions 3, 4, 5, 6.
+	 * In other words, each level contains 2^level items.
+	 */
 	public static void execute() {
 		numKeys = Integer.parseInt( properties.getProperty( "number_keys"));
 	    nodeReadCost = Integer.parseInt( properties.getProperty( "node_read_cost"));
@@ -116,22 +139,115 @@ public class TreeTraversal {
 		// For this demo, every node will have a key (String) or node data (no string)
 		System.out.println();
 		System.out.println( "Calculated values:");
-		int numNodes = 0;
+		numNodes = 0;
 		for ( int level = 0; level < numKeys; level++ ) {
 			numNodes += (int) Math.pow( 2.0,  numKeys ); // 2^0 + 2^1 + ... + 2^keys 
 		}
 		System.out.println( "Tree number nodes=" + numNodes  );
 		System.out.println( "Tree empty size=" + numNodes * nodeEmptySize );
-		int popNodes = numNodes * treeFillPercent / 100;
+		popNodes = numNodes * treeFillPercent / 100;
 		System.out.println( "Tree populated nodes=" + popNodes );
+		System.out.println( "Tree populated node size (popNodes*nodeDataSize)=" + popNodes * nodeDataSize );
 		
-		// For each key, a * represents a  level number. 
-		// A key name represents a 1.
-		// So, for example, key1..key4, there can be 
-		List<String> nodes = new ArrayList<String>( numNodes ); 
+		nodes = new ArrayList<String>( numNodes ); 
+
+		System.out.println();
+		sparseTreeTest();
+		
+		System.out.println();
+		refTreeTest();
+		
+	}
+
+	public static void sparseTreeTest() {
+		System.out.println( "Sparse Tree values:");
+		System.out.println( "Sparse tree empty node size (emptyNodes*emptyNodeSize)=" + (numNodes - popNodes) * nodeEmptySize );
+		System.out.println( "Sparse tree total node size (popSize + emptySize)=" + (numNodes - popNodes) * nodeEmptySize + (popNodes * nodeDataSize) );
+		
+		// Empty tree
+		nodes.clear();
+		nodes.add( DATA ); // Always populate default node.
+		for( int i = 0; i < numNodes - 1; i++ ) {
+			nodes.add( "" );
+		}
+
+		// Populate random nodes. Ensure no collisions. May take a while for full trees.
+		int placedData = popNodes;
+		while ( placedData > 0) {
+			int randomIndex = random.nextInt( numNodes );
+			if ( !DATA.equals( nodes.get( randomIndex ) )) {
+				nodes.set( randomIndex, DATA );
+				placedData--;
+			}
+		}
+
+		int numReads = 0;
+		// Generate numAccesses random accesses and count reads.
+		int leafPositions = (int) Math.pow( 2.0, numKeys - 1 );
+		for ( int access = 0; access < numAccesses; access++ ) {
+			// For each key (numKeys), check the leaf node for data. No data, access parent
+			int pos = random.nextInt( leafPositions );
+			int level = numKeys - 1;
+			int index = levelPositionToIndex( level, pos );
+			numReads += 1;
+			while ( (level > 0 ) && !DATA.equals( nodes.get( index ))) {
+				level -= 1;
+				pos /= 2;
+				index = levelPositionToIndex( level, pos );
+				numReads += 1;
+			}
+		}
+		System.out.println( "Sparse tree populated nodes=" + popNodes + "/" + numNodes + "(" + treeFillPercent + "%), data accesses=" + numAccesses + ", num reads=" + numReads + ", avgAccessCount="  + Double.toString( ((double)numReads)/((double)numAccesses) ));
+		
+
+	}
+
+	public static void refTreeTest() {
+		System.out.println( "Reference Tree values:");
+		System.out.println( "Ref tree key node size (keyNodes*keyNodeSize)=" + (numNodes - popNodes) * nodeKeySize );
+		System.out.println( "Ref tree total node size (popSize + keySize)=" + (numNodes - popNodes) * nodeKeySize + (popNodes * nodeDataSize) );
+		System.out.println( "Ref tree populated nodes=" + popNodes + "/" + numNodes + "(" + treeFillPercent + "%), data accesses=" + numAccesses + ", num reads=" + 1.5*numAccesses + ", avgAccessCount="  + Double.toString( 1.5 ));
 		
 	}
 	
+	/** Given an index into a list, returns a pair that has level and element number in that level.
+	 * By using Map.Entry, the key contains the level number, the value contains the element position on that level.
+	 * This assumes a binary tree.
+	 * The first level (level 0) contains item 0.
+	 * The second level (level 1) contains items 1 and 2.
+	 * The third level (level 2) contains items 3,4, 5,6.
+	 */
+	public static Map.Entry<Integer,Integer> indexToLevelPosition( int index ) {
+		if ( index < 0 ) return null;
+		int nodeCount = 0;
+		for ( int level = 0; level < numKeys; level++ ) {
+            int nodesThisLevel = (int) Math.pow( 2.0, level );
+			if ( nodeCount + nodesThisLevel  > index ) {
+				return new AbstractMap.SimpleImmutableEntry<Integer,Integer>( level, index - nodeCount  );
+			}
+			nodeCount += nodesThisLevel; 
+		}
+		return null;
+	}
+
+	/** Given an index into a list, returns a pair that has level and element number in that level.
+	 * By using Map.Entry, the key contains the level number, the value contains the element number.
+	 * This assumes a binary tree.
+	 * The first level (level 0) contains item 0.
+	 * The second level (level 1) contains items 1 and 2.
+	 * The third level (level 2) contains items 3,4, 5,6.
+	 */
+	public static int levelPositionToIndex( int level, int position ) {
+		if ( level < 0 ) return -1;
+		if (( position < 0 ) || (position >= (int) Math.pow( 2.0, level ))) return -1;
+
+		int index = 0;
+		for ( int i = 0; i < level; i++ ) {
+            index += (int) Math.pow( 2.0, i );
+		}
+		return index + position;
+	}
+
 	// For example, to convert 10 minutes to milliseconds, use: TimeUnit.MILLISECONDS.convert(10L, TimeUnit.MINUTES)
     public static String format(long durationMillis) {
         if (durationMillis == 0) return "00:00:00,000";
--- src/test/java/info/danbecker/tree/TreeTraversalTest.java
+++ src/test/java/info/danbecker/tree/TreeTraversalTest.java
@@ -1,14 +1,73 @@
 package info.danbecker.tree;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNull;
+
+import java.util.Map;
 
 public class TreeTraversalTest {
 	
 	@Test
-    public void testTreeTraversal() {
-		assertTrue( "cleaned copy directory exact size", 0 == 0 );
+    public void testLevelPosition() {		
+		TreeTraversal.numKeys = 4;
+		
+		assertNull( "index", TreeTraversal.indexToLevelPosition( -20 )  );
+		
+		Map.Entry<Integer,Integer> info = TreeTraversal.indexToLevelPosition( 0 );
+		assertTrue( "level 0,0", 0 == info.getKey() );
+		assertTrue( "level 0,0", 0 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 1 );
+		assertTrue( "level 1,0", 1 == info.getKey() );
+		assertTrue( "level 1,0", 0 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 2 );
+		assertTrue( "level 1,1", 1 == info.getKey() );
+		assertTrue( "level 1,1", 1 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 3 );
+		assertTrue( "level 2,0", 2 == info.getKey() );
+		assertTrue( "level 2,0", 0 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 4 );
+		assertTrue( "level 2,1", 2 == info.getKey() );
+		assertTrue( "level 2,1", 1 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 5 );
+		assertTrue( "level 2,2", 2 == info.getKey() );
+		assertTrue( "level 2,2", 2 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 6 );
+		assertTrue( "level 2,3", 2 == info.getKey() );
+		assertTrue( "level 2,3", 3 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 7 );
+		assertTrue( "level 3,0", 3 == info.getKey() );
+		assertTrue( "level 3,0", 0 == info.getValue() );
+
+		info = TreeTraversal.indexToLevelPosition( 14 );
+		assertTrue( "level 3,0", 3 == info.getKey() );
+		assertTrue( "level 3,0", 7 == info.getValue() );	
 	}
 	
+	@Test
+    public void testIndex() {		
+		TreeTraversal.numKeys = 4;
+		
+		assertTrue( "negative", -1 == TreeTraversal.levelPositionToIndex( -1, 0 )  );
+		assertTrue( "negative", -1 == TreeTraversal.levelPositionToIndex( 0, 1 )  );
+		assertTrue( "negative", -1 == TreeTraversal.levelPositionToIndex( 1, 2 )  );
+
+		assertTrue( "index", 0 == TreeTraversal.levelPositionToIndex( 0, 0 )  );
+		assertTrue( "index", 1 == TreeTraversal.levelPositionToIndex( 1, 0 )  );
+		assertTrue( "index", 2 == TreeTraversal.levelPositionToIndex( 1, 1 )  );
+		assertTrue( "index", 3 == TreeTraversal.levelPositionToIndex( 2, 0 )  );
+		assertTrue( "index", 4 == TreeTraversal.levelPositionToIndex( 2, 1 )  );
+		assertTrue( "index", 5 == TreeTraversal.levelPositionToIndex( 2, 2 )  );
+		assertTrue( "index", 6 == TreeTraversal.levelPositionToIndex( 2, 3 )  );
+		assertTrue( "index", 7 == TreeTraversal.levelPositionToIndex( 3, 0 )  );
+		assertTrue( "index", 14 == TreeTraversal.levelPositionToIndex( 3, 7 )  );
+	}
 }
